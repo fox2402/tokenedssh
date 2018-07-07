4203bf930acd89ae15de80517dee0648a3424299
Kenan <kenan.lejosne@gmail.com>
2018-07-07 04:30:21 +0200

    server: adds main

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,15 @@
 
 int main(int argc, char** argv)
 {
-  (void) argc;
-  (void) argv;
+  if (argc > 1 && argc < 2)
+  {
+    Server s("16555", argv[1]);
+    s.begin_listen();
+  }
+  else
+  {
+    Server s("16555");
+    s.begin_listen();
+  }
   return 0;
 }
